9eca341119e25a29ba1ee2ba53f6b2827195f3c6
Genesisnbq <genesis.nbq@gmail.com>
2021-10-03 14:54:21 +0800

    Hello world

---
+++ rust-learning/src/bin/00_hello_world.rs
@@ -0,0 +1,10 @@
+/*
+ * @Author: Binqi Ni
+ * @Date: 2021-09-09 23:55:26
+ * @LastEditTime: 2021-09-09 23:55:26
+ * @LastEditors: Binqi Ni
+ * @FilePath: /Rust-Programming/rust-learning/src/bin/00_hello_world.rs
+ */
+fn main() {
+    println!("Hello world");
+}
